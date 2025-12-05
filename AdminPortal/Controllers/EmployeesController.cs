0ff6c4a798ce0cd13ef57805d942ce37ee923a47
Doğukan YILDIZ <dogukanyildiz99@gmail.com>
2025-12-05 22:32:33 +0300

    Added Controller with Get and Post methods and created AddEmployeeDto

---
+++ AdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using AdminPortal.Data;
+using AdminPortal.Models;
+using AdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPortal.Controllers
+{
+    // route = localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployess = dbContext.Employees.ToList();
+
+            return Ok(allEmployess);
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+    }
+}
